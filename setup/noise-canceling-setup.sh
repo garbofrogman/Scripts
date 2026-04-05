7218d66bb966d507883f44d745d80b14e32e7b79
grog <deuce_cackle.1s@icloud.com>
2026-04-05 12:39:17 -0400

    Lower mic level for replays

--- setup/noise-canceling-setup.sh
+++ setup/noise-canceling-setup.sh
@@ -13,10 +13,27 @@ wget -O ~/.ladspa/libdeep_filter_ladspa.so https://github.com/Rikorose/DeepFilte
 sed -i "/args = {/a\            node.name \= \"noise_cancel\""
 # Edit plugin path
 sed -i "s/plugin =.*/plugin = \/home\/george\/.ladspa\/libdeep_filter_ladspa.so/" $PIPEWIRE/deepfilter-mono-source.conf
-# Set attenuation limit
-sed -i "s/(dB)\" .*/(dB)\" 20/" $PIPEWIRE/deepfilter-mono-source.conf
+# Set attenuation limit (100 is default/ maximum)
+sed -i "s/(dB)\" .*/(dB)\" 100/" $PIPEWIRE/deepfilter-mono-source.conf
+# Set maximum volume (because mic was super loud)
+sed -i "/capture.props = /a\                channelmix.max-volume = 0.5" $PIPEWIRE/deepfilter-mono-source.conf
 
 systemctl restart --user pipewire.service
 
 # Start this with:
 # pipewire -c filter-chain.conf
+#
+# Available properties (found using `pw-cli e input.noise_cancel 2` while this filter-chain config is in use)
+#
+#   String "DeepFilter Mono:Attenuation Limit (dB)"
+#   Float 20.000000
+#   String "DeepFilter Mono:Min processing threshold (dB)"
+#   Float -15.000000
+#   String "DeepFilter Mono:Max ERB processing threshold (dB)"
+#   Float 35.000000
+#   String "DeepFilter Mono:Max DF processing threshold (dB)"
+#   Float 35.000000
+#   String "DeepFilter Mono:Min Processing Buffer (frames)"
+#   Float 0.000000
+#   String "DeepFilter Mono:Post Filter Beta"
+#   Float 0.000000
---
+++ wip/gpu-screen-record.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Idk i want to use gpu-screen-recorder to record screen so I can get audio, but how to stop the recording when done? idkidk
+gpu-screen-recorder -w portal -o $(date +%Y-%m-%d).mp4
